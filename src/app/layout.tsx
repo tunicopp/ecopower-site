3462a861eccf60d878e7f2bc93da2025495493c7
eduardo <juniorcose@gmail.com>
2023-08-07 17:04:06 -0300

    section video

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br" className="">
+    <html lang="pt-br">
       <body className={golos_text.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SectionMap from "@/components/SectionMap/SectionMap";
 import Hero from "../components/Hero/Hero";
 import SectionSlide from "@/components/SectionSlide/SectionSlide";
+import SectionVideo from "@/components/SectionVideo/SectionVideo";
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
       <Hero />
       <SectionSlide />
       <SectionMap />
+      <SectionVideo />
+      <div className="h-screen w-full">asasasasasasas</div>
     </main>
   );
 }
