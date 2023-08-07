60b0f80beea9b8edca60feb0a62173ca4c5a41b3
willyamdev <willyamvinicius1900@gmail.com>
2023-08-07 16:19:22 -0300

    Improve layout and add blog section to the home page
    ```
    src/app/page.tsx: Add import for BlogSection component
    src/app/page.tsx: Modify Home component to include BlogSection component
    
    src/components/global/GridContainer.tsx: Adjust max width to fit larger layout
    
    ```

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import SectionMap from "@/components/SectionMap/SectionMap";
 import Hero from "../components/Hero/Hero";
 import SectionSlide from "@/components/SectionSlide/SectionSlide";
+import BlogSection from "@/components/blog-section/BlogSection";
 
 export default function Home() {
-  return (
-    <main className="">
-      <Hero />
-      <SectionSlide />
-      <SectionMap />
-    </main>
-  );
+    return (
+        <main className="">
+            <Hero/>
+            <SectionSlide/>
+            <SectionMap/>
+            <BlogSection/>
+        </main>
+    );
 }
