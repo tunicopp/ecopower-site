3348e5f624191052a1c64aa862b4ed5b836d9849
Nicolas Lemos <nicolasrlemos2001@gmail.com>
2023-08-25 15:38:58 -0300

    added faq content

--- src/utils/faqData.ts
+++ src/utils/faqData.ts
@@ -1,37 +1,40 @@
 export const faqData = [
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title: "O que é a EcoPower e como ela está relacionada à energia solar?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "A EcoPower é uma empresa líder no setor de energia solar, especializada na instalação de sistemas de geração de energia solar para residências, empresas e agronegócio.",
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title:
+      "Quais são os benefícios da energia solar proporcionados pela EcoPower?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "A energia solar oferecida pela Ecopower reduz significativamente os custos de eletricidade, é ambientalmente amigável, requer manutenção mínima e aumenta o valor das propriedades.",
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title: "Como funciona a instalação de painéis solares pela EcoPower?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "A Ecopower realiza uma avaliação do local, projeta o sistema adequado às necessidades do cliente, instala os painéis solares e realiza a conexão ao sistema elétrico, permitindo a geração de energia limpa a partir da luz solar.",
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title:
+      "Quais são as opções de financiamento oferecidas pela EcoPower para a energia solar?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "A EcoPower oferece diversas opções de financiamento, incluindo leasing solar, empréstimos de energia solar e opções de compra, tornando mais acessível a adoção da energia solar.",
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title:
+      "Como a energia solar da EcoPower pode reduzir minha conta de eletricidade?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "Ao gerar sua própria eletricidade a partir do sol, você reduzirá a dependência da rede elétrica, o que resultará em contas de eletricidade mais baixas ou até mesmo na eliminação delas.",
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title: "A energia solar da EcoPower funciona durante as quedas de energia?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "Em situações normais, os sistemas de energia solar da Ecopower estão conectados à rede elétrica e podem desligar em caso de quedas de energia. No entanto, sistemas de armazenamento de energia, como baterias, podem ser instalados para fornecer energia durante as quedas de energia.",
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur?",
+    title: "Qual é a vida útil dos sistemas de energia solar da EcoPower?",
     description:
-      "Lorem ipsum dolor sit amet consectetur. Massa lobortis orci leo lectus pellentesque enim pharetra sit phasellus. Tristique nulla faucibus enim velit aliquam dolor. Aliquet dui id elit praesent pellentesque tellus tellus at pellentesque. At sem pellentesque adipiscing tortor enim ultricies nullam bibendum in. Sagittis ac pellentesque sed dignissim dictum et eu faucibus ut.",
+      "Os sistemas de energia solar da Ecopower têm uma vida útil média de 25 a 30 anos, com garantias que cobrem o desempenho dos painéis solares durante esse período.",
   },
 ];
