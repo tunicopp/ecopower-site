870db6d57ddb4147c8e116a95effd2167125b1b8
willyamrb <willyamvinicius1900@gmail.com>
2023-10-27 09:54:30 -0300

    removed section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
         <SectionMap />
         <SectionVideo />
         <SectionCampaings />
-        <SectionSolutions />
+        {/*<SectionSolutions />*/}
         <SectionProject />
         <SectionTestimonial />
         <BlogSection />
