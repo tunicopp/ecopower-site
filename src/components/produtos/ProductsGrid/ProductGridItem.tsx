20d36a1aa670c4f66e70d30c27318e3b49796c58
willyamrb <will@atom6studio.com>
2024-05-13 16:18:45 -0300

    feat: Add link prop to ProductGridItem component
    The `ProductGridItem` component now accepts a `link` prop, which allows specifying a custom link for each product item. This enables the ability to navigate to different pages or external URLs for each product.

--- src/components/produtos/ProductsGrid/ProductGridItem.tsx
+++ src/components/produtos/ProductsGrid/ProductGridItem.tsx
@@ -8,23 +8,33 @@ interface Props {
   image: string | StaticImageData;
   title: string;
   text: string;
+  link?: string;
 }
 
-const ProductGridItem: React.FC<Props> = ({ image, text, title }) => {
+const ProductGridItem: React.FC<Props> = ({
+  image,
+  text,
+  title,
+  link = "https://api.whatsapp.com/send?phone=5517988418893&text=Ol%C3%A1,%20acabei%20de%20simular%20meu%20projeto%20no%20site%20e%20quero%20mais%20informa%C3%A7%C3%B5es",
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="flex flex-col rounded-2xl bg-beige-300 p-6">
-      <Image
-        src={image}
-        alt={title}
-        quality={100}
-        width={240}
-        height={225}
-        className="w-full max-h-[225px] object-cover"
-      />
+      <Link href={link}>
+        <Image
+          src={image}
+          alt={title}
+          quality={100}
+          width={240}
+          height={225}
+          className="w-full max-h-[225px] object-cover"
+        />
+      </Link>
       <div className="mt-6 mb-5">
-        <h2 className="text-[22px] leading-7 font-bold">{title}</h2>
+        <Link href={link} className="text-[22px] leading-7 font-bold">
+          {title}
+        </Link>
         <h3 className="text-lg my-6">{text}</h3>
         <span
           className="text-sm leading-7 font-bold cursor-pointer"
@@ -35,13 +45,7 @@ const ProductGridItem: React.FC<Props> = ({ image, text, title }) => {
       </div>
       <Link
         className="mt-auto flex items-center justify-center simulate-button min-w-[165px]"
-        href={
-          title === "EcoPower Mobi"
-            ? "ecopower-mobi"
-            : title === "EcoPower Home"
-            ? "ecopower-home"
-            : `https://api.whatsapp.com/send?phone=5517988418893&text=Ol%C3%A1,%20acabei%20de%20simular%20meu%20projeto%20no%20site%20e%20quero%20mais%20informa%C3%A7%C3%B5es`
-        }
+        href={link}
       >
         Saiba mais
       </Link>
--- src/components/produtos/ProductsGrid/ProductsGrid.tsx
+++ src/components/produtos/ProductsGrid/ProductsGrid.tsx
@@ -12,66 +12,77 @@ const products = [
     title: "EcoPower Mobi",
     text: "A EcoPower Mobi é a linha de mobilidade elétrica da EcoPower, queremos garantir a mobilidade urbana com consciência ecológica e independência financeira.",
     type: ["all", "hybrid", "connected", "disconnected"],
+    link: "ecopower-mobi",
   },
   {
     image: "/assets/images/produtos/banner-home.png",
     title: "EcoPower Home",
     text: "Torne seu o lar um ambiente mais inteligente, aconchegante e com equipamentos para facilitar a sua vida.",
     type: ["all", "hybrid", "connected", "disconnected"],
+    link: "ecopower-home",
   },
   {
     image: "/assets/images/produtos/kit-meu-solar.png",
     title: "Kit Meu Solar",
     text: "É a oportunidade de levar até as famílias, a viabilidade de praticamente eliminar um dos principais custos fixos de uma casa, a energia elétrica.",
     type: ["all", "connected", "hybrid"],
+    link: "kit-meu-solar",
   },
   {
     image: "/assets/images/produtos/cota-credito.png",
     title: "Cota de Crédito",
     text: "As cotas do título são reservadas de acordo com a necessidade de crédito mensal médio, o cliente faz a adesão e passa a receber os créditos mensalmente",
     type: ["all", "connected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/investidor.png",
     title: "Investidor",
     text: "Produza energia solar de forma sustentável, gere créditos de energia limpa e venda esses créditos conforme os meios previstos e regulamentados.",
     type: ["all", "connected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/off-grid.png",
     title: "Off Grid",
     text: "É um sistema isolado, que não está conectado à rede de distribuição elétrica. Geralmente utilizado em regiões onde não há eletricidade ou distribuição elétrica.",
     type: ["all", "disconnected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/locacao-de-area.png",
     title: "Locação de Área",
     text: "Arrende seu terreno de até 1000m² em até 25 anos para a montagem e produção de uma usina de energia solar. Ideal para a renda da agricultura familiar.",
     type: ["all", "connected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/vale-energia.png",
     title: "Vale Energia",
     text: "Contribua com seus colaboradores a não pagar pelo uso de energia elétrica em suas residências enquanto trabalham com você.",
     type: ["all", "connected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/bombeamento-solar.png",
     title: "Bombeamento Solar",
     text: "O cliente utiliza a mesma bomba d’agua que ele já utilizava e o acionamento do motor passa a ser realizado através da energia gerada pelos painéis solares.",
     type: ["all", "disconnected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/power-livre.png",
     title: "Power Livre",
     text: "Produza energia no modelo zero grid e atenda o auto consumo. Para o consumo em horários onde não possui geração de energia, continue no mercado livre.",
     type: ["all", "disconnected"],
+    link: undefined,
   },
   {
     image: "/assets/images/produtos/cliente-trifasico.png",
     title: "Cliente Trifásico",
     text: "Entendemos sua demanda, consumo e qualidade da energia na sua empresa ou fazenda. Nosso kit solar trifásico garante eficiência ao cliente.",
     type: ["all", "hybrid", "connected", "disconnected"],
+    link: undefined,
   },
 ];
 
@@ -158,6 +169,7 @@ const ProductsGrid: React.FC = () => {
               image={p.image}
               text={p.text}
               title={p.title}
+              link={p.link}
             />
           ))}
       </div>
