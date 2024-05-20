1eaf10794b5a161dc22208c7f09a11d04615060e
raphael <rapha.guasta@gmail.com>
2024-05-19 23:31:07 -0300

    Add boxes to EnergiaEmCidade

--- src/components/EnergiaEmCidade/SectionValeAPena.tsx
+++ src/components/EnergiaEmCidade/SectionValeAPena.tsx
@@ -8,49 +8,109 @@ import tree from "../../../public/assets/images/hero/tree-2.png";
 import cloudOne from "../../../public/assets/images/hero/cloud-1.png";
 import cloudTwo from "../../../public/assets/images/hero/cloud-2.png";
 import cloudThree from "../../../public/assets/images/hero/cloud-2.png";
+import planetIcon from "../../../public/assets/images/energia-em-cidade/icone-planeta.svg";
+import energyIcon from "../../../public/assets/images/energia-em-cidade/icone-energia.svg";
+import moneyIcon from "../../../public/assets/images/energia-em-cidade/icone-energia.svg";
 
 interface CityInterface {
-    city?: string;
+  city?: string;
 }
 
 const SectionValeAPena: React.FC<CityInterface> = ({ city }) => {
   return (
     <>
-        <section className="min-h-[250px] flex flex-col items-center justify-center bg-hero-yellow relative overflow-hidden">
+      <section className="min-h-[250px] flex flex-col items-center justify-center bg-hero-yellow relative overflow-hidden">
         <GridContainer className="flex-col justify-center items-center">
-            <TitleComponent className="text-black">Vale a pena energia solar em {city}?</TitleComponent>
+          <TitleComponent className="text-black">
+            Vale a pena energia solar em {city}?
+          </TitleComponent>
         </GridContainer>
         <div className="absolute -right-[-40px] lg:-bottom-[24px] md:-bottom-[50px] -bottom-[0px]">
-            <Image
+          <Image
             src={panel}
             alt="Painel Solar"
             className="md:w-[407px] w-[180px]"
-            />
+          />
         </div>
         <div className="absolute lg:left-[156px] left-[40px] bottom-[30px]">
-            <Image
+          <Image
             src={cloudOne}
             alt="Núvem"
             className="md:w-[129px] md:h-[36px] w-[80px]"
-            />
+          />
         </div>
         <div className="absolute lg:right-[563px] md:right-[300px] right-[50px] lg:top-[81px] top-[50px]">
-            <Image
+          <Image
             src={cloudTwo}
             alt="Núvem"
             className="md:w-[97px] md:h-[27px] w-[60px]"
-            />
+          />
         </div>
         <div className="absolute right-[10px] bottom-[2px]">
-            <Image
+          <Image
             src={cloudThree}
             alt="Núvem"
             className="md:w-[129px] md:h-[36px] w-[80px]"
-            />
+          />
         </div>
+      </section>
+      <div className="text-3xl leading-[30px] text-black mt-[40px] text-center">
+        <span className="font-bold">- Sim!</span> Veja porque:
+      </div>
+      <GridContainer className="flex-col">
+        <section className="bg-white">
+          <div className="grid md:grid-cols-3 mt-[40px] gap-6">
+            <div className="flex item-center px-6 py-8 bg-beige-300 rounded-2xl place-content-around">
+              <Image
+                src={planetIcon}
+                className="mr-4"
+                alt="Sistemas Fotovoltaicos Instalados Ícone"
+                width={50}
+                height={50}
+                quality={100}
+              />
+              <div className="max-w-[70%]">
+                <h3 className="text-xl mt-2 mb-2 font-medium text-grayscale-400">
+                  Mais de {"todo 1000"} sistemas fotovoltaicos foram instalados
+                  em {city}.
+                </h3>
+              </div>
+            </div>
+            <div className="flex item-center px-6 py-8 bg-beige-300 rounded-2xl place-content-around">
+              <Image
+                src={energyIcon}
+                className="mr-4"
+                alt="Conta de Luz Média"
+                width={50}
+                height={50}
+                quality={100}
+              />
+              <div className="max-w-[70%]">
+                <h3 className="text-xl mt-2 mb-2 font-medium text-grayscale-400">
+                  A conta de luz média em {city} é de {"to do valor"}.
+                </h3>
+              </div>
+            </div>
+            <div className="flex item-center px-6 py-8 bg-beige-300 rounded-2xl place-content-around">
+              <Image
+                src={moneyIcon}
+                className="mr-4"
+                alt="Principal Distribuidora Ícone"
+                width={50}
+                height={50}
+                quality={100}
+              />
+              <div className="max-w-[70%]">
+                <h3 className="text-xl mt-2 mb-2 font-medium text-grayscale-400">
+                  A principal distribuidora em {city} é a {"to do nome"}.
+                </h3>
+              </div>
+            </div>
+          </div>
+          <br />
+          <br />
         </section>
-        <section>
-        </section>
+      </GridContainer>
     </>
   );
 };
